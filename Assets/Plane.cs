54af3dce535fb67bb48316a55860d13c93e8552a
洪金敏 <jonny.hong91@gmail.com>
2023-05-11 01:37:52 +0800

    更新例子

--- Assets/Plane.cs
+++ Assets/Plane.cs
@@ -22,30 +22,35 @@ public class Plane : MonoBehaviour
     private Transform EffectL;
     private Transform EffectR;
 
-    private Vector3 _offset = new Vector3(0, 0, -.5f);
+    private Vector3 _offset = new Vector3(0, 0, 0.1f);
     private Quaternion _originRot;
 
     private void Start()
     {
         _originRot = EffectPrefab.localRotation;
         EffectL = Instantiate(EffectPrefab, OtherSpace);
-        EffectR = Instantiate(EffectPrefab, OtherSpace);
+        EffectR = Instantiate(EffectPrefab,OtherSpace);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         EffectL.transform.position = L.position + _offset;
         EffectR.transform.position = R.position + _offset;
+        EffectL.transform.rotation = L.rotation;
+        EffectR.transform.rotation = R.rotation;
 
         // var posWorldL = L.localToWorldMatrix.MultiplyPoint3x4(_offset);
         // EffectL.transform.localPosition = OtherSpace.worldToLocalMatrix.MultiplyPoint3x4(posWorldL);
+        // EffectL.transform.localRotation = Quaternion.Euler(_originRot.eulerAngles + L.rotation.eulerAngles - OtherSpace.eulerAngles);
         //
         // var posWorldR = R.localToWorldMatrix.MultiplyPoint3x4(_offset);
         // EffectR.transform.localPosition = OtherSpace.worldToLocalMatrix.MultiplyPoint3x4(posWorldR);
+        // EffectR.transform.localRotation = Quaternion.Euler(_originRot.eulerAngles + R.rotation.eulerAngles - OtherSpace.eulerAngles);
+        
       
         // var posWorldL = L.TransformPoint(_offset);
         // EffectL.transform.localPosition = OtherSpace.InverseTransformPoint(posWorldL);
-        // EffectL.transform.localRotation = _originRot * Quaternion.Inverse(OtherSpace.rotation);
+        // EffectL.transform.localRotation = _originRot * Quaternion.Inverse(OtherSpace.rotation) * L.rotation;
     }
     
     
